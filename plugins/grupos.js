ebd368a93f9e26f9fa3b4728f92dc247451e2a47
Yander <103792433+Theyanderbot@users.noreply.github.com>
2022-04-16 17:09:50 -0600

    Update grupos.js

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -3,29 +3,14 @@ let fs = require('fs')
 let handler  = async (m, { conn, usedPrefix: _p }) => {
   
 let info = `
-*Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de The Shadow Borkers - Bot*
+*Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o conviértete en su bot *
 
 *➤ Grupos oficiales del Bot:*
 *1.-* https://chat.whatsapp.com/HvpipeAxhIQ6maQPJturId
 
-*2.-* https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs
+*2.-* https://chat.whatsapp.com/Iy9ckMakugL7zp39gXvQzo
 
-*3.-* https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F
 
-*4.-* https://chat.whatsapp.com/CjexkGVr37J6GuSdDVAHzC
-
-*5.-* https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb
-
-*6.-* https://chat.whatsapp.com/GvrLSUSzVuT9P17CKfdxDa
-
-*7.-* https://chat.whatsapp.com/EpzuymKm6lG08k6J2Dwx7F
-
-*8.-* https://chat.whatsapp.com/IW12dLVoyWGHreGpX7rQIw
-
-*9.-* https://chat.whatsapp.com/Ef89aIuOLeD3Fa2sYmKwp7
-
-*10.-* https://chat.whatsapp.com/C2WYb1hiiijCI13QSUZLM9
-`.trim() 
 
 conn.sendMessage(m.chat, info, MessageType.text, { quoted: { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net', fromMe: false }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": '🔥 THE SHADOW BROKERS - BOT 🔥', "jpegThumbnail": fs.readFileSync(`./Menu2.jpg`)}}}})}
 
