9c20cf70999b9193a93a31bc2c030837d0f4cc45
Yander <103792433+Theyanderbot@users.noreply.github.com>
2022-04-16 17:20:50 -0600

    Update grupos.js

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -3,13 +3,17 @@ let fs = require('fs')
 let handler  = async (m, { conn, usedPrefix: _p }) => {
   
 let info = `
-*Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o conviértete en su bot *
-
+*Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de Theyander- Bot*
 *➤ Grupos oficiales del Bot:*
 *1.-* https://chat.whatsapp.com/HvpipeAxhIQ6maQPJturId
 *2.-* https://chat.whatsapp.com/Iy9ckMakugL7zp39gXvQzo
 
 
+
+
+
+
+`.trim() 
 
 conn.sendMessage(m.chat, info, MessageType.text, { quoted: { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net', fromMe: false }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": '🔥 THE SHADOW BROKERS - BOT 🔥', "jpegThumbnail": fs.readFileSync(`./Menu2.jpg`)}}}})}
 
