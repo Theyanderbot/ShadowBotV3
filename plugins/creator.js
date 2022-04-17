45aad77c675226094c7c5f6482aabcf20f7f77d3
Yander <103792433+Theyanderbot@users.noreply.github.com>
2022-04-17 13:23:24 -0600

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 function handler(m) {
 this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
-this.sendContact(m.chat, '50671762786', 'Shadow - Creador - Oficial 1', m)
-this.sendContact(m.chat, '18454424766', 'Shadow - Creador - Oficial 2', m)}
+this.sendContact(m.chat, '50671762786', 'BOT - Creador - Oficial 1', m)
+this.sendContact(m.chat, '18454424766', 'BOT - Creador - Oficial 2', m)}
 handler.command = /^(contacto|owner|creator|creador|propietario|dueño)$/i
 module.exports = handler
